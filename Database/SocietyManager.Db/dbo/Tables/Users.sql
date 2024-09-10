4bec864b20ad97c325d3cd9759fb2f31e8b9c4b0
KierenLWoods <kierenlwoods@gmail.com>
2024-09-10 16:48:23 +0100

    Add user table

---
+++ Database/SocietyManager.Db/dbo/Tables/Users.sql
@@ -0,0 +1,14 @@
+CREATE TABLE [dbo].[Users]
+(
+  	[UserId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [EmailAddress] NVARCHAR(256) NOT NULL,     
+    [Password] VARCHAR(MAX) NOT NULL, 
+    [UserName] VARCHAR(15) NOT NULL, 
+    [Active] BIT NOT NULL DEFAULT 1, 
+    [IsConfirmed] BIT NOT NULL DEFAULT 0, 
+    [CreatedDate] DATETIME NOT NULL DEFAULT getutcdate(), 
+    [VerificationData] VARCHAR(50) NULL, 
+    [PasswordChangeGuid] NVARCHAR(50) NULL, 
+)
